dd515691464f3c992049c2c3d5b387ac594b95cd
Arthur <arthur.rochette@gmail.com>
2021-07-26 15:26:09 +0200

    set to 3d , finish 3 forces

--- js/boids.js
+++ js/boids.js
@@ -2,14 +2,20 @@ import * as THREE from './three.module.js';
 import {Vector2, Vector3} from "./three.module.js";
 
 //VAR PARAMETERS
-let width = 250;
-let height = 275;
-let depth = 50;//todo auto
+let width = 200;
+let height = 200;
+let depth = 500;//todo perspective camera area lookin
 
-let BOIDS_NUMBER = 150;
-let VISION = 5;
-let ALIGNFORCE = 0.01;
+let BOIDS_NUMBER = 300;
+let VISION = 15;
+let SEPARATION_DISTANCE = 10;
+let ALIGN_FORCE = 0.1;
+let COHESION_FORCE = 0.1;
+let SEPARATION_FORCE = 0.1;
+let ROTATION_FORCE = 0.1;
+let SPEED = 0.5;
 
+//todo make a var to limit movement in a direction (like gravity)
 
 
 //CLASS
@@ -30,41 +36,93 @@ class Boid extends THREE.Mesh {
         const wireframe = new THREE.LineSegments(wireframeGeo, wireframeMat);
         wireframe.name = "wireframe";
         this.add(wireframe)
-        this._velocity = new Vector2().random();
+        this._velocity = new Vector3().random();
         this._velocity = this._velocity.subScalar(0.5);
         this._velocity.clampLength(Math.random(), Math.random() + 1);
-        this._acceleration = new Vector2();
+        this._acceleration = new Vector3();
 
     }
 
     update() {
         this.position.add(this._velocity);
-        this.position.z = 0;//todo 3d
+
         this._velocity.add(this._acceleration)
+        this._velocity.clampLength(-SPEED, SPEED);
     }
 
     align(boids) {
-        let average = new Vector2();
+        let average = new Vector3();
         let nbr = 0;
         for (let boid of boids) {
             let distance = boid.position.distanceTo(this.position)
-
             if (distance < VISION && boid != this) {
                 nbr++;
                 average.add(boid._velocity);
-
             }
-
         }
         if (nbr > 0) {
             average.divideScalar(nbr);
+            average.setLength(SPEED);//magnitude
             average.sub(this._velocity);
-            average.min(new Vector2(ALIGNFORCE, ALIGNFORCE));
-            average.max(new Vector2(-ALIGNFORCE, -ALIGNFORCE));
+            average.min(new Vector3(ALIGN_FORCE, ALIGN_FORCE, ALIGN_FORCE));
+            average.max(new Vector3(-ALIGN_FORCE, -ALIGN_FORCE, -ALIGN_FORCE));
         }
         return average;
     }
 
+    cohesion(boids){
+        let average = new Vector3();
+        let nbr = 0;
+        for (let boid of boids) {
+            let distance = boid.position.distanceTo(this.position)
+            if (distance < VISION && boid != this) {
+                nbr++;
+                average.add(boid.position);
+            }
+        }
+        if (nbr > 0) {
+            average.divideScalar(nbr);
+            average.sub(this.position);
+            average.setLength(SPEED)
+            average.min(new Vector3(COHESION_FORCE, COHESION_FORCE, COHESION_FORCE));
+            average.max(new Vector3(-COHESION_FORCE, -COHESION_FORCE, -COHESION_FORCE));
+        }
+        return average;
+    }
+
+    separation(boids){
+        let average = new Vector3();
+        let nbr = 0;
+        for (let boid of boids) {
+            let distance = boid.position.distanceTo(this.position)
+            if (distance < SEPARATION_DISTANCE && boid != this) {
+                let diff = new THREE.Vector3().subVectors(this.position, boid.position);
+                diff.multiplyScalar(distance);//invertional prop
+                average.add(diff);
+                nbr++;
+            }
+        }
+        if (nbr > 0) {
+            average.divideScalar(nbr);
+            average.setLength(SPEED)
+            average.min(new Vector3(SEPARATION_FORCE, SEPARATION_FORCE, SEPARATION_FORCE));
+            average.sub(this._velocity);
+            average.max(new Vector3(-SEPARATION_FORCE, -SEPARATION_FORCE, -SEPARATION_FORCE));
+        }
+        return average;
+    }
+
+    rotateCenter(){
+
+    }
+
+    computeForces(boids) {
+        this._acceleration.multiplyScalar(0); // set to 0
+        this._acceleration = this.separation(boids);
+        this._acceleration.add(this.align(boids));
+        this._acceleration.add(this.cohesion(boids));
+
+    }
 
 
     limits(){
@@ -80,13 +138,11 @@ class Boid extends THREE.Mesh {
             this.position.y = height/2;
         }
 
-
-
-
-    }
-
-    computeForces(boids) {
-        this._acceleration = this.align(boids);
+        if(this.position.z > camera.position.z+5){
+            this.position.z = camera.position.z - depth;
+        }else if(this.position.z < camera.position.z - depth){
+            this.position.z = camera.position.z +5;
+        }
 
     }
 
@@ -109,9 +165,9 @@ document.body.appendChild(renderer.domElement);
 let boids = [];
 for (let i = 0; i < BOIDS_NUMBER; i++) {
     boids.push(new Boid());
-    boids[i].position.x = Math.random() * 50;
-    boids[i].position.y = Math.random() * 50;
-    boids[i].position.z = Math.random() * 0;
+    boids[i].position.x = Math.random() * 200;
+    boids[i].position.y = Math.random() * 200;
+    boids[i].position.z = Math.random() * 200;
 
     scene.add(boids[boids.length - 1]);
 }
@@ -121,8 +177,8 @@ for (let i = 0; i < BOIDS_NUMBER; i++) {
 function animate() {
     requestAnimationFrame(animate);
     for (let boid of boids) {
-        boid.computeForces(boids);
         boid.limits();
+        boid.computeForces(boids);
         boid.update();
 
     }
